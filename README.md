da412549aa5cc0e3c24cb477a39c54f4181d8811
mrjuiceofc <133717102+mrjuiceofc@users.noreply.github.com>
2023-08-26 20:25:39 +0200

    Bay AI

---
+++ README.md
@@ -0,0 +1,63 @@
+# DREADED-AI GPT
+
+***This bot is created with the help of NodeJS and uses [Baileys](https://github.com/adiwajshing/Baileys)***
+
+## FEATURES
+There are 2 features, getting an AI response to your query by using the command ```!g``` and getting an AI generated image by using the command ```!img```
+
+## Find  OpenAI ApiKey and Change it in Required.json
+- Get openAi key [Here](https://beta.openai.com/account/api-keys) and paste it where relevant!
+</h2>
+ 
+     
+<br>
+     <br>
+     
+
+     
+<h2 align="center">   Run Repl and scan QR Code
+</h2>
+<h2 align="center">  <a href="https://replit.com/@fortunedreaded/DREADED-QR-SCANNER#"><img src="https://repl.it/badge/github/quiec/whatsasena" />
+</a>
+</h2>
+   <br>
+   </br>
+
+
+<h2 align="center">  Deploy Via Heroku  
+</h2>
+
+<p align="center" >
+    <a href="https://heroku.com/deploy?template=https://github.com/Fortunatusmokaya/DREADED-GPT-AI">
+    <img title="DREADED on Heroku" src="https://www.herokucdn.com/deploy/button.png" width="189px" alt="Deploy on Heroku" >
+    </a>
+</p>
+     
+
+
+</br>     
+
+**Install Via Termux ✅**
+
+Manually install ```node_modules``` or use the command ```npm install``` to install them although you may get error if your terminal is not well configured.
+<br>The ```node_modules``` are [here](https://drive.google.com/file/d/1xgRIwDVuTklxwdtsx933WfmzqtRxEsGV/view?usp=share_link)
+```bash
+$ pkg install git nodejs -y
+$ git clone https://github.com/Fortunatusmokaya/DREADED-GPT-AI
+$ cd DREADED-GPT-AI
+If you did not install the node modules manually then proceed with the command below.
+$ npm install
+```
+Extract contents of node_modules, from ```node_modules.zip``` to the folder DREADED-AI.
+
+**Run**
+```bash
+$ node index.js
+```
+
+
+## License
+[MIT License](https://github.com/Fortunatusmokaya/DREADED-AI/blob/main/LICENSE)
+
+Copyright (c) 2023 Dreaded AI-ChatGpt
+
---
+++ dreaded.js
@@ -0,0 +1,384 @@
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require("@adiwajshing/baileys");
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const { fetchUrl, isUrl, processTime } = require("./lib/dreadfunc");
+const { Configuration, OpenAIApi } = require("openai");
+let setting = process.env.AI; 
+
+module.exports = dreaded = async (client, m, chatUpdate, store) => {
+  try {
+    var body =
+      m.mtype === "conversation"
+        ? m.message.conversation
+        : m.mtype == "imageMessage"
+        ? m.message.imageMessage.caption
+        : m.mtype == "videoMessage"
+        ? m.message.videoMessage.caption
+        : m.mtype == "extendedTextMessage"
+        ? m.message.extendedTextMessage.text
+        : m.mtype == "buttonsResponseMessage"
+        ? m.message.buttonsResponseMessage.selectedButtonId
+        : m.mtype == "listResponseMessage"
+        ? m.message.listResponseMessage.singleSelectReply.selectedRowId
+        : m.mtype == "templateButtonReplyMessage"
+        ? m.message.templateButtonReplyMessage.selectedId
+        : m.mtype === "messageContextInfo"
+        ? m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text
+        : "";
+    var budy = typeof m.text == "string" ? m.text : "";
+    
+    const prefix = process.env.PREFIX;
+    const cmd = body.startsWith(prefix);
+    const command = body.replace(prefix, "").trim().split(/ +/).shift().toLowerCase();
+    const args = body.trim().split(/ +/).slice(1);
+    const pushname = m.pushName || "No Name";
+    const botNumber = await client.decodeJid(client.user.id);
+    const itsMe = m.sender == botNumber ? true : false;
+    let text = (q = args.join(" "));
+    const arg = budy.trim().substring(budy.indexOf(" ") + 1);
+    const arg1 = arg.trim().substring(arg.indexOf(" ") + 1);
+
+    const from = m.chat;
+    const reply = m.reply;
+    const sender = m.sender;
+    const mek = chatUpdate.messages[0];
+    const getGroupAdmins = (participants) => { 
+       let admins = []; 
+       for (let i of participants) { 
+         i.admin === "superadmin" ? admins.push(i.id) : i.admin === "admin" ? admins.push(i.id) : ""; 
+       } 
+       return admins || []; 
+     };
+    const fortu = (m.quoted || m); 
+         const quoted = (fortu.mtype == 'buttonsMessage') ? fortu[Object.keys(fortu)[1]] : (fortu.mtype == 'templateMessage') ? fortu.hydratedTemplate[Object.keys(fortu.hydratedTemplate)[1]] : (fortu.mtype == 'product') ? fortu[Object.keys(fortu)[0]] : m.quoted ? m.quoted : m; 
+ 
+
+    const color = (text, color) => {
+      return !color ? chalk.green(text) : chalk.keyword(color)(text);
+    };
+const dev = process.env.DEV; 
+ const DevDreaded = dev.split(",");
+    const badwordkick = process.env.BAD_WORD_KICK;
+   const bad = process.env.BAD_WORD;
+    const autoreadrecord = process.env.AUTOREAD_AND_TYPE;
+    const badword = bad.split(",");
+    const Owner = DevDreaded.map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(m.sender)
+    // Group
+    const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch((e) => {}) : "";
+    const groupName = m.isGroup ? groupMetadata.subject : "";
+    const participants = m.isGroup ? await groupMetadata.participants : ""; 
+     const groupAdmin = m.isGroup ? await getGroupAdmins(participants) : ""; 
+     const isBotAdmin = m.isGroup ? groupAdmin.includes(botNumber) : false; 
+     const isAdmin = m.isGroup ? groupAdmin.includes(m.sender) : false;
+const admin = process.env.ADMIN_MSG;
+    const group = process.env.GROUP_ONLY_MSG;
+    const botAdmin = process.env.BOT_ADMIN_MSG;
+    const NotOwner = process.env.NOT_OWNER_MSG;
+    // Push Message To Console
+    let argsLog = budy.length > 30 ? `${q.substring(0, 30)}...` : budy;
+    if (autoreadrecord === 'TRUE' && !m.isGroup) { 
+             client.readMessages([m.key]); 
+  client.sendPresenceUpdate('composing', m.chat);
+    }
+    if (m.chat.endsWith("@s.whatsapp.net")) {
+
+  	
+
+  if (!text) return reply("I need more text please. Make your query a bit longer.");
+
+           const configuration = new Configuration({
+
+              apiKey: setting,
+
+            });
+
+            const openai = new OpenAIApi(configuration);
+
+            try {
+
+const response = await openai.createChatCompletion({
+
+          model: "gpt-3.5-turbo",
+
+          messages: [{role: "user", content: text}],
+
+          });
+
+          m.reply(`${response.data.choices[0].message.content}`);
+
+          } catch (error) {
+
+          if (error.response) {
+
+            console.log(error.response.status);
+
+            console.log(error.response.data);
+
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+
+          } else {
+
+            console.log(error);
+
+            m.reply("I\'m Facing An Error:"+ error.message);
+
+          }
+
+          }
+
+    }
+if (badwordkick === 'TRUE' && isBotAdmin && !isAdmin && body && (new RegExp('\\b' + badword.join('\\b|\\b') + '\\b')).test(body.toLowerCase())) {
+            
+     client.groupParticipantsUpdate(from, [sender], 'remove')
+            reply("Removed\n\nMr Juice Prohibits Use Of Bad Words In The Bot Presence!")
+            
+        
+                                                   }
+    if (!Owner && isBotAdmins && !isAdmins && m.isGroup && (isUrl(m.text))) { 
+  
+ kid = m.sender; 
+  
+ client.sendMessage(m.chat, { 
+  
+                delete: { 
+                   remoteJid: m.chat, 
+                   fromMe: false, 
+                   id: m.key.id, 
+                   participant: kid 
+                } 
+             }).then(() => client.groupParticipantsUpdate(m.chat, [kid], 'remove')); 
+ client.sendMessage(m.chat, {text:`Removed\n\n@${kid.split("@")[0]} sending links is prohibited by Bot Owner!`, contextInfo:{mentionedJid:[kid]}}, {quoted:m}); 
+       }   
+  
+  
+  
+ 
+
+    if (cmd && !m.isGroup) {
+      console.log(chalk.black(chalk.bgWhite("[ DREADED-AI ]")), color(argsLog, "turquoise"), chalk.magenta("From"), chalk.green(pushname), chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`));
+    } else if (cmd && m.isGroup) {
+      console.log(
+        chalk.black(chalk.bgWhite("[ LOGS ]")),
+        color(argsLog, "turquoise"),
+        chalk.magenta("From"),
+        chalk.green(pushname),
+        chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`),
+        chalk.blueBright("IN"),
+        chalk.green(groupName)
+      );
+    }
+
+    if (cmd) {
+      switch (command) {
+        case "help":
+        case "menu":
+
+                       client.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/d6dab955fbaa42fce2280.jpg' }, caption: `BAYMAX BOT\n\nHello ${m.pushName}.\nThis is BayMax AI Bot, a simple whatsApp Bot! 
+  
+  THIS IS A PUBLIC BOT AND DEVELOPMENT IS ON PROGRESS
+ 
+  
+  
+       ------- 𝐸𝑛𝑑 ------- 
+  
+               𝗡𝗼𝘁𝗲: 
+  
+ - BayMax AI Is A Artificial Intelligence bot Created for Educational purposes only.
+ - Do not call or spam the bot! 🦄 
+ - Donot use the bot for Political or Nudity purposes l
+ - Bot is still under development so expect random bugs and errors! 🔧 
+ 
+   
+    
+ `, fileLength: "9999999999999999999999"}, { quoted: m }); 
+           break;
+          m.reply(`This Public bot is under development.`)
+          // Group Commands
+          break;
+          case "admin" : { 
+                 if (!m.isGroup) throw group; 
+         if (!isBotAdmin) throw botAdmin; 
+          if (!Owner) throw NotOwner; 
+                 await client.groupParticipantsUpdate(m.chat,  [m.sender], 'promote'); 
+ m.reply('Promoted To Admin<🥇'); 
+          }
+          break;
+ case "close": case "mute": { 
+  
+                 if (!m.isGroup) throw group; 
+                 if (!isBotAdmin) throw botAdmin; 
+                 if (!isAdmin) throw admin; 
+  
+                     await client.groupSettingUpdate(m.chat, 'announcement'); 
+ m.reply('Group successfully locked!'); 
+ } 
+ break; 
+ case "open": case "unmute": { 
+                 if (!m.isGroup) throw group; 
+                 if (!isBotAdmin) throw botAdmin; 
+                 if (!isAdmin) throw admin; 
+  
+                     await client.groupSettingUpdate(m.chat, 'not_announcement'); 
+ m.reply('Group successfully unlocked!'); 
+  
+ }
+          case "disp1": { 
+                 if (!m.isGroup) throw group; 
+                 if (!isBotAdmin) throw botAdmin; 
+                 if (!isAdmin) throw admin; 
+  
+                     await client.groupToggleEphemeral(m.chat, 1*24*3600); 
+ m.reply('Dissapearing messages successfully turned on for 24hrs!'); 
+ } 
+ break; 
+
+          case "promote" : { 
+                 if (!m.isGroup) throw group; 
+         if (!isBotAdmin) throw botAdmin; 
+         if (!isAdmin) throw admin; 
+ if (!m.quoted) throw `Tag someone with the command!`; 
+                 let users = m.mentionedJid[0] ? m.mentionedJid : m.quoted ? [m.quoted.sender] : [text.replace(/[^0-9]/g, '')+'@s.whatsapp.net']; 
+  
+                 await client.groupParticipantsUpdate(m.chat, users, 'promote'); 
+ m.reply('Successfully promoted! 👑'); 
+         } 
+ break; 
+ case "demote": { 
+                 if (!m.isGroup) throw group; 
+         if (!isBotAdmin) throw botAdmin; 
+         if (!isAdmin) throw admin; 
+ if (!m.quoted) throw `Tag someone with the command!`; 
+                 let users = m.mentionedJid[0] ? m.mentionedJid : m.quoted ? [m.quoted.sender] : [text.replace(/[^0-9]/g, '')+'@s.whatsapp.net']; 
+  
+                 await client.groupParticipantsUpdate(m.chat, users, 'demote'); 
+ m.reply('Successfully demoted! 🎗️'); 
+         } 
+ break;
+ case "disp7": { 
+                 if (!m.isGroup) throw group; 
+                 if (!isBotAdmin) throw botAdmin; 
+                 if (!isAdmin) throw admin; 
+  
+                     await client.groupToggleEphemeral(m.chat, 7*24*3600); 
+ m.reply('Dissapearing messages successfully turned on for 7 days!'); 
+  
+ } 
+ break; 
+ case "disp90": { 
+                 if (!m.isGroup) throw group; 
+                 if (!isBotAdmin) throw botAdmin; 
+                 if (!isAdmin) throw admin; 
+  
+                     await client.groupToggleEphemeral(m.chat, 90*24*3600); 
+ m.reply('Dissapearing messages successfully turned on for 90 days!'); 
+ } 
+ break; 
+ case "disp-off": { 
+                 if (!m.isGroup) throw group; 
+                 if (!isBotAdmin) throw botAdmin; 
+                 if (!isAdmin) throw admin; 
+  
+                     await client.groupToggleEphemeral(m.chat, 0); 
+ m.reply('Dissapearing messages successfully turned off!'); 
+ }
+        case "g": case "openai": 
+          
+
+            if (!text) return reply("I need more text please. Make your query a bit longer.");
+
+           const configuration = new Configuration({
+
+              apiKey: setting,
+
+            });
+
+            const openai = new OpenAIApi(configuration);
+
+            try {
+
+const response = await openai.createChatCompletion({
+
+          model: "gpt-3.5-turbo",
+
+          messages: [{role: "user", content: text}],
+
+          });
+
+          m.reply(`${response.data.choices[0].message.content}`);
+
+          } catch (error) {
+
+          if (error.response) {
+
+            console.log(error.response.status);
+
+            console.log(error.response.data);
+
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+
+          } else {
+
+            console.log(error);
+
+            m.reply("I\'m Facing An Error:"+ error.message);
+
+          }
+
+            }
+          
+          break;
+        case "img": case "ai-img": case "image": case "images":
+          try {
+            if (setting === "ADD OPENAI API KEY") return reply("I need an openAi API key in my .env file.");
+            if (!text) return reply(`This will generate an AI-BASED image. Note that image generated might not be realistic.`);
+            const configuration = new Configuration({
+              apiKey: setting,
+            });
+            const openai = new OpenAIApi(configuration);
+            const response = await openai.createImage({
+              prompt: text,
+              n: 1,
+              size: "512x512",
+            });
+            //console.log(response.data.data[0].url)
+            client.sendImage(from, response.data.data[0].url, text, mek);
+            } catch (error) {
+          if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+          } else {
+            console.log(error);
+            m.reply("An error has occurred:"+ error.message);
+          }
+        }
+break;
+        default: {
+          if (cmd && budy.toLowerCase() != undefined) {
+            if (m.chat.endsWith("broadcast")) return;
+            if (m.isBaileys) return;
+            if (!budy.toLowerCase()) return;
+            if (argsLog || (cmd && !m.isGroup)) {
+              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
+              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("Dreaded", "turquoise"));
+            } else if (argsLog || (cmd && m.isGroup)) {
+              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
+              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("Dreaded", "turquoise"));
+            }
+          }
+        }
+      }
+    }
+  } catch (err) {
+    m.reply(util.format(err));
+  }
+};
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update ${__filename}`));
+  delete require.cache[file];
+  require(file);
+});
